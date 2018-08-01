e4f36743916410669b959d2f22481980c6e0450d
15707901327 <1429648501@qq.com>
2018-08-01 14:31:08 +0800

    1.控制播放背景音乐实例

---
+++ audio/audio.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+  var musicControl = new PUtils.MusicControl("../music/mp3/AL Rocco、小安迪 - China Wave (Live).mp3", {});
+  musicControl.justPlay();
+};
---
+++ js/PUtils.js
@@ -0,0 +1,81 @@
+var PUtils = PUtils || {};
+
+(function (PUtils) {
+  /**
+   * html结构
+   * <span id="musicControl">
+   *   <a id="mc_play" class="stop" onclick="play_music();">
+   *     <audio id="musicfx" loop="loop">
+   *       <source src="../music/mp3/AL Rocco、小安迪 - China Wave (Live).mp3" type="audio/mpeg">
+   *     </audio>
+   *   </a>
+   * </span>
+   * @param url 播放的路径
+   * @param options
+   *  container：挂载点 《body》
+   *  autoplay：是否自动播放 false
+   * @constructor
+   */
+  PUtils.MusicControl = function (url, options) {
+    options = options || {};
+
+    this.url = url;
+    this.container = options.container !== undefined ? options.container : document.getElementsByTagName("body")[0];
+    this.autoplay = options.autoplay !== undefined ? options.autoplay : false;
+
+    this.aNode = document.createElement("a");
+    this.audioNode = document.createElement("audio");
+
+    this._createDom();
+  };
+  Object.assign(PUtils.MusicControl.prototype, {
+    // 创建dom结构
+    _createDom: function () {
+      var controlNode = document.createElement("div");
+      controlNode.id = "musicControl";
+
+      this._playMusic = this._bind(this, this.playMusic);
+      this.aNode.addEventListener("click",this._playMusic);
+
+      this.audioNode.autoplay = this.autoplay;
+
+      var sourceNode = document.createElement("source");
+      sourceNode.src = this.url;
+      sourceNode.type = "audio/mpeg";
+
+      this.audioNode.appendChild(sourceNode);
+      this.aNode.appendChild(this.audioNode);
+      controlNode.appendChild(this.aNode);
+      this.container.appendChild(controlNode);
+    },
+    /**
+     * 播放音乐
+     */
+    justPlay: function () {
+      this.audioNode.play();
+      this.aNode.setAttribute("class", "on");
+    },
+    /**
+     * 播放音乐
+     */
+    justPause: function () {
+      this.audioNode.pause();
+      this.aNode.setAttribute("class", "stop");
+    },
+    /**
+     * 控制播放音乐
+     */
+    playMusic: function () {
+      if (this.aNode.getAttribute("class") === "on") {
+        this.justPause();
+      } else {
+        this.justPlay();
+      }
+    },
+    _bind: function (scope, fn) {
+      return function () {
+        fn.apply(scope, arguments);
+      };
+    }
+  });
+})(PUtils);
